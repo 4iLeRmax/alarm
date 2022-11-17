282c08c5aeeae7a88427eec921fd5eb5dbcefee0
4iLeRmax <102809824+4iLeRmax@users.noreply.github.com>
2022-11-17 13:08:19 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,57 @@
-// let wrapper = document.querySelector('.wrapper');
+window.onload = ()=>{
+
+  let body = document.body;
+  let ss = document.querySelector('.styleSwitcher');
+  
+  ss.onclick = ()=>{
+    if(!ss.classList.contains('active')){
+      ss.classList.add('active');
+      body.classList.add('dark');
+      body.classList.remove('light');
+    }
+    else if(ss.classList.contains('active')){
+      ss.classList.remove('active');
+      body.classList.add('light');
+      body.classList.remove('dark');
+    }
+  }
+
+
+
+  // let wrapper = document.querySelector('.wrapper');
+let black = document.querySelector('.black');
 let card = document.querySelector('.card');
 let cardHeader = document.querySelector('.card__header');
 let switcher = document.querySelector('.switcher');
 let btn = document.querySelector('.done');
 
 switcher.onclick = ()=>{
-  switcher.classList.toggle('active');
-}
-cardHeader.onclick = ()=>{
-  card.classList.add('active');
-}
-btn.onclick = ()=>{
-  if(card.classList.contains('active')){
-    card.classList.remove('active');
-  }
   if(!switcher.classList.contains('active')){
     switcher.classList.add('active');
   }
+  else{
+    switcher.classList.remove('active');
+  }
 }
 
 
-// wrapper.onclick=()=>{
-//   card.classList.remove('active');
-// }
+cardHeader.onclick = ()=>{
+  if(!card.classList.contains('active')){
+    card.classList.add('active');
+  }
+  if(!black.classList.contains('active')){
+    black.classList.add('active');
+  }
+}
+black.onclick = ()=>{
+  if(card.classList.contains('active')){
+    card.classList.remove('active');
+  }
+  if(black.classList.contains('active')){
+    black.classList.remove('active');
+  }
+}
+
 
 
 
@@ -31,7 +60,7 @@ let hourLine = document.querySelector('.hour');
 let minutesLine = document.querySelector('.minutes');
 let y1;
 let y2;
-// let count;
+
 let scrollDown;
 let scrollUp;
 let val;
@@ -42,15 +71,18 @@ let number;
 
 let timeH = document.querySelector('.time .timeHour');
 let timeM = document.querySelector('.time .timeMinutes');
+let time;
+let cycle = document.querySelector('.cycle');
+
+
 
 hourLine.ontouchstart = (e)=>{
   y1 = e.touches[0].clientY;
-  console.log(y1);
+  // console.log(y1);
 }
-
 hourLine.ontouchmove = (e)=>{
   y2 = e.touches[0].clientY;
-  console.log(y2);
+  // console.log(y2);
 
   scrollDown = Math.round((y1-y2)/10);
   scrollUp = Math.round((y2-y1)/10);
@@ -58,13 +90,13 @@ hourLine.ontouchmove = (e)=>{
   // scrollUp = Math.round((y2/10)-scrollDown);
 
   if(y1>y2){
-    console.log('scrollDown to ' + scrollDown);
+    // console.log('scrollDown to ' + scrollDown);
     if(scrollDown<=21 && scrollDown>=0){
       hourLine.style.top = -(scrollDown * 65) + 'px';
     }
   }
   else if(y1<y2){
-    console.log('scrollUp to ' + scrollUp);
+    // console.log('scrollUp to ' + scrollUp);
     if(scrollUp>=21 && scrollUp<=0){
       hourLine.style.top = (scrollUp * 65) + 'px';
     }
@@ -72,7 +104,7 @@ hourLine.ontouchmove = (e)=>{
 
 
   val = Number(scrollDown) + 2;
-  console.log('val '+ val);
+  // console.log('val '+ valH);
   if(val<10 && val>0){
     timeH.innerHTML =  `0${val}`;
   }
@@ -85,10 +117,19 @@ hourLine.ontouchmove = (e)=>{
   for(let i=0; i<hours.length; i++){
     number = Number(hours[i].firstChild.data);
     // console.log('num '+number);
-    if((number-2)==scrollDown){
-      hours[i].style.color = '#fff';
-    } else{
-      hours[i].style.color = '#777';
+    if(body.classList.contains('dark')){
+      if((number-2)==scrollDown){
+        hours[i].style.color = '#fff';
+      } else{
+        hours[i].style.color = '#777';
+      }
+    }
+    else if(body.classList.contains('light')){
+      if((number-2)==scrollDown){
+        hours[i].style.color = '#333';
+      } else{
+        hours[i].style.color = '#777';
+      }
     }
   }
 }
@@ -97,12 +138,11 @@ hourLine.ontouchmove = (e)=>{
 
 minutesLine.ontouchstart = (e)=>{
   y1 = e.touches[0].clientY;
-  console.log(y1);
+  // console.log(y1);
 }
-
 minutesLine.ontouchmove = (e)=>{
   y2 = e.touches[0].clientY;
-  console.log(y2);
+  // console.log(y2);
 
   scrollDown = Math.round((y1-y2)/5);
   scrollUp = Math.round((y2-y1)/5);
@@ -110,13 +150,13 @@ minutesLine.ontouchmove = (e)=>{
   // scrollUp = Math.round((y2/10)-scrollDown);
 
   if(y1>y2){
-    console.log('scrollDown to ' + scrollDown);
+    // console.log('scrollDown to ' + scrollDown);
     if(scrollDown<=57 && scrollDown>=0){
       minutesLine.style.top = -(scrollDown * 65) + 'px';
     }
   }
   else if(y1<y2){
-    console.log('scrollUp to ' + scrollUp);
+    // console.log('scrollUp to ' + scrollUp);
     if(scrollUp>=57 && scrollUp<=0){
       minutesLine.style.top = (scrollUp * 65) + 'px';
     }
@@ -124,7 +164,7 @@ minutesLine.ontouchmove = (e)=>{
 
 
   val = Number(scrollDown) + 2;
-  console.log('val '+ val);
+  // console.log('val '+ valM);
   if(val<10 && val>0){
     timeM.innerHTML =  `0${val}`;
   }
@@ -136,20 +176,79 @@ minutesLine.ontouchmove = (e)=>{
   for(let i=0; i<minutes.length; i++){
     number = Number(minutes[i].firstChild.data);
     // console.log('num '+number);
-    if((number-2)==scrollDown){
-      minutes[i].style.color = '#fff';
-    } else{
-      minutes[i].style.color = '#777';
+    if(body.classList.contains('dark')){
+      if((number-2)==scrollDown){
+        minutes[i].style.color = '#fff';
+      } else{
+        minutes[i].style.color = '#777';
+      }
+    }
+    else if(body.classList.contains('light')){
+      if((number-2)==scrollDown){
+        minutes[i].style.color = '#333';
+      } else{
+        minutes[i].style.color = '#777';
+      }
     }
   }
 }
 
 
+function timeConverter(item){
+  let a = item;
+  let hour = a.getHours();
+  if(hour<10){
+    hour = `0${hour}`;
+  }
+  let min = a.getMinutes();
+  if(min<10){
+    min = `0${min}`;
+  }
+  time = `${hour}:${min}`;
+  return time;
+}
+function timeDiff(firstDate, secondDate){
+  let getDate = (string) => new Date(0, 0,0, string.split(':')[0], string.split(':')[1]); //получение даты из строки (подставляются часы и минуты
+  let different = (getDate(secondDate) - getDate(firstDate));
+  let hour;
+  let minute;
+  if(different>0){
+    hour = Math.floor((different % 86400000) / 3600000);
+    minute = Math.round(((different % 86400000) % 3600000) / 60000);
+  }
+  else if(different<0){
+    hour = 24 - (-Number(Math.floor((different % 86400000) / 3600000)));
+    minute = 60 - (-Number(Math.round(((different % 86400000) % 3600000) / 60000)));
+  }
 
-let day_1 = new Date(2021, 06, 01),
-    day_2 = new Date(2021, 05, 31);
 
-function diffDates(day_one, day_two) {
-    return (day_one - day_two) / (60 * 60 *24 * 1000);
-};
-console.log(diffDates(day_1, day_2));
+  let result = `${hour} ч ${minute} мин`;
+  if(minute=="00"){
+    result = `${hour} ч`;
+  }
+  return result;
+}
+function addDiffTime(){
+  let firstDate = timeConverter(new Date);
+  // console.log(firstDate);
+
+  let secondDate = `${timeH.textContent}:${timeM.textContent}`;
+  // console.log(secondDate);
+
+  cycle.innerHTML=`Через ${timeDiff(firstDate, secondDate)}`;
+  
+}
+btn.onclick = ()=>{
+  if(card.classList.contains('active')){
+    card.classList.remove('active');
+  }
+  if(!switcher.classList.contains('active')){
+    switcher.classList.add('active');
+  }
+  if(black.classList.contains('active')){
+    black.classList.remove('active');
+  }
+  addDiffTime();
+}
+
+}
